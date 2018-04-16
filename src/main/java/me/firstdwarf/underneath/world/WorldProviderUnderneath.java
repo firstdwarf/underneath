5967cf8c8ffec1f9538fa0b2017ae09a6b64a32b
Blake Hamilton <blakeh2999@gmail.com>
2018-04-16 00:11:41 -0500

    Fixed gitignore and README files

--- src/main/java/me/firstdwarf/underneath/world/WorldProviderUnderneath.java
+++ src/main/java/me/firstdwarf/underneath/world/WorldProviderUnderneath.java
@@ -1,14 +1,9 @@
 package me.firstdwarf.underneath.world;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.WorldProvider;
 import net.minecraft.world.gen.IChunkGenerator;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class WorldProviderUnderneath extends WorldProvider {
 
@@ -40,18 +35,6 @@ public class WorldProviderUnderneath extends WorldProvider {
     @Override
     public BlockPos getSpawnPoint() {
         return new BlockPos(0, 0, 0); // Make sure we always spawn at X = 0, Y = 0, & Z = 0 for testing
-    }
-
-    /**
-     * When a player is added to this world provider (goes to the underneath world)
-     *
-     * @param player The player
-     */
-    // TODO: REMOVE THIS -- this is for debug only
-    @SideOnly(Side.CLIENT)
-    @Override
-    public void onPlayerAdded(EntityPlayerMP player) {
-        Minecraft.getMinecraft().player.sendMessage(new TextComponentString("Welcome to the Underneath!"));
     }
 
 }
