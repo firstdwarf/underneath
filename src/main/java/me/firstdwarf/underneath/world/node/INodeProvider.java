c641fc6026c29f9da63440e33282d9b565cca911
firstdwarf <j.a.y@me.com>
2018-08-31 21:13:04 -0500

    flagNeighbors moved to default method in interface

--- src/main/java/me/firstdwarf/underneath/world/node/INodeProvider.java
+++ src/main/java/me/firstdwarf/underneath/world/node/INodeProvider.java
@@ -2,6 +2,7 @@ package me.firstdwarf.underneath.world.node;
 
 import java.util.ArrayList;
 
+import me.firstdwarf.underneath.utilities.Functions;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
@@ -15,6 +16,127 @@ public interface INodeProvider {
 	 * Number of tunnels, entrance coordinates, entrance direction
 	 */
 	public ArrayList<Entrance> getEntrances();
+	public BlockPos[] getBounds();
 	public String getName();
-	public void flagNeighbors(ChunkPos chunkPos, BlockPos nodeOrigin, int rotation);
+	public default void flagNeighbors(ChunkPos chunkPos, BlockPos nodeOrigin, int nodeRotation)	{
+		BlockPos chunkMax = Functions.nodeCoordsToChunkCoords(this.getBounds()[1], nodeOrigin, nodeRotation);
+		BlockPos chunkMin = Functions.nodeCoordsToChunkCoords(this.getBounds()[0], nodeOrigin, nodeRotation);
+		int maxXReach, minXReach, maxZReach, minZReach;
+		maxXReach = (int) ((chunkMax.getX() >= 0) ? Math.floor((chunkMax.getX())/16.0) : Math.floor((chunkMax.getX()/16.0)));
+		minXReach = (int) ((chunkMin.getX() >= 0) ? Math.floor((chunkMin.getX())/16.0) : Math.floor((chunkMin.getX()/16.0)));
+		maxZReach = (int) ((chunkMax.getZ() >= 0) ? Math.floor((chunkMax.getZ())/16.0) : Math.floor((chunkMax.getZ()/16.0)));
+		minZReach = (int) ((chunkMin.getZ() >= 0) ? Math.floor((chunkMin.getZ())/16.0) : Math.floor((chunkMin.getZ()/16.0)));
+		//System.out.println("Values of max and min: " + chunkMax.toString() + "    " + chunkMin.toString());
+		//System.out.println("xMax: " + maxXReach +  " xMin: " + minXReach + " zMax: " + maxZReach + " zMin: " + minZReach);
+		ArrayList<ChunkPos> flaggedChunks = new ArrayList<>();
+		//Flag chunks to the chunkMax corner
+		if (maxXReach >= 0)	{
+			if (maxZReach >= 0) {
+				for (int i = 0; i <= maxXReach; i++)	{
+					for (int j = 0; j <= maxZReach; j++)	{
+						if (i != 0 || j != 0)	{
+							flaggedChunks.add(new ChunkPos(chunkPos.x + i, chunkPos.z + j));
+						}
+					}
+				}
+			}
+			else if (maxZReach < 0)	{
+				for (int i = 0; i <= maxXReach; i++)	{
+					for (int j = 0; j >= maxZReach; j--)	{
+						if (i != 0 || j != 0)	{
+							flaggedChunks.add(new ChunkPos(chunkPos.x + i, chunkPos.z + j));
+						}
+					}
+				}
+			}
+		}
+		else if (maxXReach < 0)	{
+			if (maxZReach >= 0) {
+				for (int i = 0; i >= maxXReach; i--)	{
+					for (int j = 0; j <= maxZReach; j++)	{
+						if (i != 0 || j != 0)	{
+							flaggedChunks.add(new ChunkPos(chunkPos.x + i, chunkPos.z + j));
+						}
+					}
+				}
+			}
+			else if (maxZReach < 0)	{
+				for (int i = 0; i >= maxXReach; i--)	{
+					for (int j = 0; j >= maxZReach; j--)	{
+						if (i != 0 || j != 0)	{
+							flaggedChunks.add(new ChunkPos(chunkPos.x + i, chunkPos.z + j));
+						}
+					}
+				}
+			}
+		}
+		
+		//Flag chunks to the chunkMin corner
+		if (minXReach >= 0)	{
+			if (minZReach >= 0) {
+				for (int i = 0; i <= minXReach; i++)	{
+					for (int j = 0; j <= minZReach; j++)	{
+						if (i != 0 || j != 0)	{
+							flaggedChunks.add(new ChunkPos(chunkPos.x + i, chunkPos.z + j));
+						}
+					}
+				}
+			}
+			else if (minZReach < 0)	{
+				for (int i = 0; i <= minXReach; i++)	{
+					for (int j = 0; j >= minZReach; j--)	{
+						if (i != 0 || j != 0)	{
+							flaggedChunks.add(new ChunkPos(chunkPos.x + i, chunkPos.z + j));
+						}
+					}
+				}
+			}
+		}
+		else if (minXReach < 0)	{
+			if (minZReach >= 0) {
+				for (int i = 0; i >= minXReach; i--)	{
+					for (int j = 0; j <= minZReach; j++)	{
+						if (i != 0 || j != 0)	{
+							flaggedChunks.add(new ChunkPos(chunkPos.x + i, chunkPos.z + j));
+						}
+					}
+				}
+			}
+			else if (minZReach < 0)	{
+				for (int i = 0; i >= minXReach; i--)	{
+					for (int j = 0; j >= minZReach; j--)	{
+						if (i != 0 || j != 0)	{
+							flaggedChunks.add(new ChunkPos(chunkPos.x + i, chunkPos.z + j));
+						}
+					}
+				}
+			}
+		}
+		if (!flaggedChunks.isEmpty())	{
+			for(ChunkPos c : flaggedChunks)	{
+				if (!NodeGen.chunkNodes.containsKey(c.toString()))	{
+					NodeGen.chunkNodes.put(c.toString(), -2);
+				}
+			}
+		}
+		//Store present entrances
+		for(Entrance e : this.getEntrances())	{
+			//System.out.println("Preparing to flag entrance");
+			e = e.rotateFacing(nodeRotation);
+			ArrayList<Entrance> chunkEntrances = new ArrayList<>();
+			BlockPos eWorldCoords = Functions.nodeCoordsToWorldCoords(e.coords, chunkPos, nodeOrigin, nodeRotation);
+			ChunkPos eChunk = new ChunkPos(eWorldCoords.getX() >> 4, eWorldCoords.getZ() >> 4);
+			e = e.setCoords(Functions.worldCoordsToChunkCoords(eWorldCoords));
+			if (NodeGen.chunkEntrances.containsKey(eChunk.toString()))	{
+				chunkEntrances = NodeGen.chunkEntrances.get(eChunk.toString());
+				chunkEntrances.add(e);
+				NodeGen.chunkEntrances.put(eChunk.toString(), chunkEntrances);
+			}
+			else	{
+				//System.out.println("Stored an entrance for " + eChunk.toString());
+				chunkEntrances.add(e);
+				NodeGen.chunkEntrances.put(eChunk.toString(), chunkEntrances);
+			}
+		}
+	}
 }
--- src/main/java/me/firstdwarf/underneath/world/node/Spawn.java
+++ src/main/java/me/firstdwarf/underneath/world/node/Spawn.java
@@ -12,7 +12,7 @@ import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
 
 public class Spawn implements INodeProvider	{
-	//TODO: Entrance mapping only works on game restart- as in, deleting without a restart prevents mapping. Check call location?
+	//TODO: Entrance map might only works on game restart- as in, deleting without a restart prevents mapping. Check call location?
 	//Consider node origin to be (0, 0, 0)- put this near where you want the main entrance)
 	ArrayList<Coords> coordinates = new ArrayList<>();
 	ArrayList<IBlockState> states = new ArrayList<>();
@@ -35,6 +35,12 @@ public class Spawn implements INodeProvider	{
 		Coords.recordStateCuboid(0, 0, 0, 0, -4, 4, Blocks.BRICK_BLOCK.getDefaultState(), coordinates, states);
 	}
 
+	@Override
+	public BlockPos[] getBounds()	{
+		BlockPos[] b = {new BlockPos(this.xMin, 0, this.zMin), new BlockPos(this.xMax, 0, this.zMax)};
+		return b;
+	}
+	
 	@Override
 	public void placeStructures(World world, BlockPos origin, int rotation) {
 		for (int i = 0; i < coordinates.size(); i++)	{
@@ -73,128 +79,5 @@ public class Spawn implements INodeProvider	{
 	@Override
 	public String getName()	{
 		return "spawn";
-	}
-	
-	@Override
-	public void flagNeighbors(ChunkPos chunkPos, BlockPos nodeOrigin, int nodeRotation)	{
-		BlockPos chunkMax = Functions.nodeCoordsToChunkCoords(new BlockPos(xMax, 0, zMax), nodeOrigin, nodeRotation);
-		BlockPos chunkMin = Functions.nodeCoordsToChunkCoords(new BlockPos(xMin, 0, zMin), nodeOrigin, nodeRotation);
-		int maxXReach, minXReach, maxZReach, minZReach;
-		maxXReach = (int) ((chunkMax.getX() >= 0) ? Math.floor((chunkMax.getX())/16.0) : Math.floor((chunkMax.getX()/16.0)));
-		minXReach = (int) ((chunkMin.getX() >= 0) ? Math.floor((chunkMin.getX())/16.0) : Math.floor((chunkMin.getX()/16.0)));
-		maxZReach = (int) ((chunkMax.getZ() >= 0) ? Math.floor((chunkMax.getZ())/16.0) : Math.floor((chunkMax.getZ()/16.0)));
-		minZReach = (int) ((chunkMin.getZ() >= 0) ? Math.floor((chunkMin.getZ())/16.0) : Math.floor((chunkMin.getZ()/16.0)));
-		//System.out.println("Values of max and min: " + chunkMax.toString() + "    " + chunkMin.toString());
-		//System.out.println("xMax: " + maxXReach +  " xMin: " + minXReach + " zMax: " + maxZReach + " zMin: " + minZReach);
-		ArrayList<ChunkPos> flaggedChunks = new ArrayList<>();
-		//Flag chunks to the chunkMax corner
-		if (maxXReach >= 0)	{
-			if (maxZReach >= 0) {
-				for (int i = 0; i <= maxXReach; i++)	{
-					for (int j = 0; j <= maxZReach; j++)	{
-						if (i != 0 || j != 0)	{
-							flaggedChunks.add(new ChunkPos(chunkPos.x + i, chunkPos.z + j));
-						}
-					}
-				}
-			}
-			else if (maxZReach < 0)	{
-				for (int i = 0; i <= maxXReach; i++)	{
-					for (int j = 0; j >= maxZReach; j--)	{
-						if (i != 0 || j != 0)	{
-							flaggedChunks.add(new ChunkPos(chunkPos.x + i, chunkPos.z + j));
-						}
-					}
-				}
-			}
-		}
-		else if (maxXReach < 0)	{
-			if (maxZReach >= 0) {
-				for (int i = 0; i >= maxXReach; i--)	{
-					for (int j = 0; j <= maxZReach; j++)	{
-						if (i != 0 || j != 0)	{
-							flaggedChunks.add(new ChunkPos(chunkPos.x + i, chunkPos.z + j));
-						}
-					}
-				}
-			}
-			else if (maxZReach < 0)	{
-				for (int i = 0; i >= maxXReach; i--)	{
-					for (int j = 0; j >= maxZReach; j--)	{
-						if (i != 0 || j != 0)	{
-							flaggedChunks.add(new ChunkPos(chunkPos.x + i, chunkPos.z + j));
-						}
-					}
-				}
-			}
-		}
-		
-		//Flag chunks to the chunkMin corner
-		if (minXReach >= 0)	{
-			if (minZReach >= 0) {
-				for (int i = 0; i <= minXReach; i++)	{
-					for (int j = 0; j <= minZReach; j++)	{
-						if (i != 0 || j != 0)	{
-							flaggedChunks.add(new ChunkPos(chunkPos.x + i, chunkPos.z + j));
-						}
-					}
-				}
-			}
-			else if (minZReach < 0)	{
-				for (int i = 0; i <= minXReach; i++)	{
-					for (int j = 0; j >= minZReach; j--)	{
-						if (i != 0 || j != 0)	{
-							flaggedChunks.add(new ChunkPos(chunkPos.x + i, chunkPos.z + j));
-						}
-					}
-				}
-			}
-		}
-		else if (minXReach < 0)	{
-			if (minZReach >= 0) {
-				for (int i = 0; i >= minXReach; i--)	{
-					for (int j = 0; j <= minZReach; j++)	{
-						if (i != 0 || j != 0)	{
-							flaggedChunks.add(new ChunkPos(chunkPos.x + i, chunkPos.z + j));
-						}
-					}
-				}
-			}
-			else if (minZReach < 0)	{
-				for (int i = 0; i >= minXReach; i--)	{
-					for (int j = 0; j >= minZReach; j--)	{
-						if (i != 0 || j != 0)	{
-							flaggedChunks.add(new ChunkPos(chunkPos.x + i, chunkPos.z + j));
-						}
-					}
-				}
-			}
-		}
-		if (!flaggedChunks.isEmpty())	{
-			for(ChunkPos c : flaggedChunks)	{
-				if (!NodeGen.chunkNodes.containsKey(c.toString()))	{
-					NodeGen.chunkNodes.put(c.toString(), -2);
-				}
-			}
-		}
-		//Store present entrances
-		for(Entrance e : entrances)	{
-			//System.out.println("Preparing to flag entrance");
-			e = e.rotateFacing(nodeRotation);
-			ArrayList<Entrance> chunkEntrances = new ArrayList<>();
-			BlockPos eWorldCoords = Functions.nodeCoordsToWorldCoords(e.coords, chunkPos, nodeOrigin, nodeRotation);
-			ChunkPos eChunk = new ChunkPos(eWorldCoords.getX() >> 4, eWorldCoords.getZ() >> 4);
-			e = e.setCoords(Functions.worldCoordsToChunkCoords(eWorldCoords));
-			if (NodeGen.chunkEntrances.containsKey(eChunk.toString()))	{
-				chunkEntrances = NodeGen.chunkEntrances.get(eChunk.toString());
-				chunkEntrances.add(e);
-				NodeGen.chunkEntrances.put(eChunk.toString(), chunkEntrances);
-			}
-			else	{
-				//System.out.println("Stored an entrance for " + eChunk.toString());
-				chunkEntrances.add(e);
-				NodeGen.chunkEntrances.put(eChunk.toString(), chunkEntrances);
-			}
-		}
 	}
 }
