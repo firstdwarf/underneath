fb051954d23d29948353807acda76f7e687fb39e
firstdwarf <j.a.y@me.com>
2020-07-19 20:45:45 -0400

    Minor updates

--- src/main/java/com/firstdwarf/underneath/block/Porter.java
+++ src/main/java/com/firstdwarf/underneath/block/Porter.java
@@ -5,6 +5,7 @@ import com.firstdwarf.underneath.world.DimensionWrapper;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.stats.Stats;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
@@ -12,6 +13,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.server.ServerWorld;
 
 public class Porter extends Block {
 
@@ -23,12 +25,18 @@ public class Porter extends Block {
 	public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
 		if (worldIn.isRemote) {
 	         return ActionResultType.SUCCESS;
-	    } else {
-	    	System.out.println("Click!");
-	    	if (!worldIn.isRemote) {
-	    		player.changeDimension((worldIn.dimension.getType() == DimensionWrapper.underneathTop) ? DimensionType.OVERWORLD : DimensionWrapper.underneathTop);
-	  	    }
+	    } else if (!worldIn.isRemote) {
+	    	ServerPlayerEntity sp = (ServerPlayerEntity) player;
+	    	if (!net.minecraftforge.common.ForgeHooks.onTravelToDimension(sp, DimensionWrapper.underneathTop)) return ActionResultType.SUCCESS;
+	    	
+	    	ServerWorld target = (worldIn.dimension.getType() != DimensionWrapper.underneathTop) ? sp.server.getWorld(DimensionWrapper.underneathTop) : sp.server.getWorld(DimensionType.OVERWORLD);
+	    	//BlockPos spawn = target.dimension.findSpawn((int) sp.getPosX(), (int) sp.getPosZ(), false);
+	    	BlockPos spawn = new BlockPos(100, 200, 100);
+	    	sp.teleport(target, spawn.getX() + 0.5, spawn.getY() + 1, spawn.getZ() + 0.5, sp.rotationYaw, sp.rotationPitch);
+	    	
+	    	net.minecraftforge.fml.hooks.BasicEventHooks.firePlayerChangedDimensionEvent(sp, target.dimension.getType(), target.dimension.getType());
 	        return ActionResultType.SUCCESS;
 	    }
+		return ActionResultType.SUCCESS;
 	}
 }
--- src/main/java/com/firstdwarf/underneath/world/UnderneathDimension.java
+++ src/main/java/com/firstdwarf/underneath/world/UnderneathDimension.java
@@ -33,15 +33,7 @@ import net.minecraft.world.dimension.Dimension;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.ChunkGeneratorType;
-import net.minecraft.world.gen.DebugChunkGenerator;
-import net.minecraft.world.gen.DebugGenerationSettings;
-import net.minecraft.world.gen.EndChunkGenerator;
-import net.minecraft.world.gen.EndGenerationSettings;
-import net.minecraft.world.gen.FlatChunkGenerator;
-import net.minecraft.world.gen.FlatGenerationSettings;
 import net.minecraft.world.gen.Heightmap;
-import net.minecraft.world.gen.NetherChunkGenerator;
-import net.minecraft.world.gen.NetherGenSettings;
 import net.minecraft.world.gen.OverworldChunkGenerator;
 import net.minecraft.world.gen.OverworldGenSettings;
 
@@ -55,103 +47,70 @@ public class UnderneathDimension extends Dimension {
 	public ChunkGenerator<?> createChunkGenerator() {
 		//Delicious pasta
 		WorldType worldtype = this.world.getWorldInfo().getGenerator();
-		ChunkGeneratorType<FlatGenerationSettings, FlatChunkGenerator> chunkgeneratortype = ChunkGeneratorType.FLAT;
-		ChunkGeneratorType<DebugGenerationSettings, DebugChunkGenerator> chunkgeneratortype1 = ChunkGeneratorType.DEBUG;
-		ChunkGeneratorType<NetherGenSettings, NetherChunkGenerator> chunkgeneratortype2 = ChunkGeneratorType.CAVES;
-		ChunkGeneratorType<EndGenerationSettings, EndChunkGenerator> chunkgeneratortype3 = ChunkGeneratorType.FLOATING_ISLANDS;
-		ChunkGeneratorType<OverworldGenSettings, OverworldChunkGenerator> chunkgeneratortype4 = ChunkGeneratorType.SURFACE;
+		ChunkGeneratorType<OverworldGenSettings, OverworldChunkGenerator> overworldGenerator = ChunkGeneratorType.SURFACE;
 		BiomeProviderType<SingleBiomeProviderSettings, SingleBiomeProvider> biomeprovidertype = BiomeProviderType.FIXED;
 		BiomeProviderType<OverworldBiomeProviderSettings, OverworldBiomeProvider> biomeprovidertype1 = BiomeProviderType.VANILLA_LAYERED;
 		BiomeProviderType<CheckerboardBiomeProviderSettings, CheckerboardBiomeProvider> biomeprovidertype2 = BiomeProviderType.CHECKERBOARD;
-		if (worldtype == WorldType.FLAT) {
-		   FlatGenerationSettings flatgenerationsettings = FlatGenerationSettings.createFlatGenerator(new Dynamic<>(NBTDynamicOps.INSTANCE, this.world.getWorldInfo().getGeneratorOptions()));
-		   SingleBiomeProviderSettings singlebiomeprovidersettings1 = biomeprovidertype.createSettings(this.world.getWorldInfo()).setBiome(flatgenerationsettings.getBiome());
-		   return chunkgeneratortype.create(this.world, biomeprovidertype.create(singlebiomeprovidersettings1), flatgenerationsettings);
-		} else if (worldtype == WorldType.DEBUG_ALL_BLOCK_STATES) {
-		   SingleBiomeProviderSettings singlebiomeprovidersettings = biomeprovidertype.createSettings(this.world.getWorldInfo()).setBiome(Biomes.PLAINS);
-		   return chunkgeneratortype1.create(this.world, biomeprovidertype.create(singlebiomeprovidersettings), chunkgeneratortype1.createSettings());
-		} else if (worldtype != WorldType.BUFFET) {
-		   OverworldGenSettings overworldgensettings = chunkgeneratortype4.createSettings();
-		   OverworldBiomeProviderSettings overworldbiomeprovidersettings = biomeprovidertype1.createSettings(this.world.getWorldInfo()).setGeneratorSettings(overworldgensettings);
-		   return chunkgeneratortype4.create(this.world, biomeprovidertype1.create(overworldbiomeprovidersettings), overworldgensettings);
-		} else {
-			BiomeProvider biomeprovider = null;
-		    JsonElement jsonelement = Dynamic.convert(NBTDynamicOps.INSTANCE, JsonOps.INSTANCE, this.world.getWorldInfo().getGeneratorOptions());
-		    JsonObject jsonobject = jsonelement.getAsJsonObject();
-		    JsonObject jsonobject1 = jsonobject.getAsJsonObject("biome_source");
-		    if (jsonobject1 != null && jsonobject1.has("type") && jsonobject1.has("options")) {
-		    BiomeProviderType<?, ?> biomeprovidertype3 = Registry.BIOME_SOURCE_TYPE.getOrDefault(new ResourceLocation(jsonobject1.getAsJsonPrimitive("type").getAsString()));
-		    JsonObject jsonobject2 = jsonobject1.getAsJsonObject("options");
-		    Biome[] abiome = new Biome[]{Biomes.OCEAN};
-		    if (jsonobject2.has("biomes")) {
-		    	JsonArray jsonarray = jsonobject2.getAsJsonArray("biomes");
-		        abiome = jsonarray.size() > 0 ? new Biome[jsonarray.size()] : new Biome[]{Biomes.OCEAN};
 		
-		        for(int i = 0; i < jsonarray.size(); ++i) {
-		        	abiome[i] = Registry.BIOME.getValue(new ResourceLocation(jsonarray.get(i).getAsString())).orElse(Biomes.OCEAN);
-		        }
-		    }
 		
-		    if (BiomeProviderType.FIXED == biomeprovidertype3) {
-		    	SingleBiomeProviderSettings singlebiomeprovidersettings2 = biomeprovidertype.createSettings(this.world.getWorldInfo()).setBiome(abiome[0]);
-		    	biomeprovider = biomeprovidertype.create(singlebiomeprovidersettings2);
-		    }
-		
-		    if (BiomeProviderType.CHECKERBOARD == biomeprovidertype3) {
-		    	int j = jsonobject2.has("size") ? jsonobject2.getAsJsonPrimitive("size").getAsInt() : 2;
-		    	CheckerboardBiomeProviderSettings checkerboardbiomeprovidersettings = biomeprovidertype2.createSettings(this.world.getWorldInfo()).setBiomes(abiome).setSize(j);
-		    	biomeprovider = biomeprovidertype2.create(checkerboardbiomeprovidersettings);
-		    }
-		
-		    if (BiomeProviderType.VANILLA_LAYERED == biomeprovidertype3) {
-		    	OverworldBiomeProviderSettings overworldbiomeprovidersettings1 = biomeprovidertype1.createSettings(this.world.getWorldInfo());
-		    	biomeprovider = biomeprovidertype1.create(overworldbiomeprovidersettings1);
-		    }
-		}
-		
-		if (biomeprovider == null) {
-		    biomeprovider = biomeprovidertype.create(biomeprovidertype.createSettings(this.world.getWorldInfo()).setBiome(Biomes.OCEAN));
-		}
-		
-		BlockState blockstate = Blocks.STONE.getDefaultState();
-		BlockState blockstate1 = Blocks.WATER.getDefaultState();
+		BiomeProvider biomeprovider = null;
+	    JsonElement jsonelement = Dynamic.convert(NBTDynamicOps.INSTANCE, JsonOps.INSTANCE, this.world.getWorldInfo().getGeneratorOptions());
+	    JsonObject jsonobject = jsonelement.getAsJsonObject();
+	    JsonObject jsonobject1 = jsonobject.getAsJsonObject("biome_source");
+	    if (jsonobject1 != null && jsonobject1.has("type") && jsonobject1.has("options")) {
+	    	BiomeProviderType<?, ?> biomeprovidertype3 = Registry.BIOME_SOURCE_TYPE.getOrDefault(new ResourceLocation(jsonobject1.getAsJsonPrimitive("type").getAsString()));
+	    	JsonObject jsonobject2 = jsonobject1.getAsJsonObject("options");
+	    	Biome[] abiome = new Biome[]{Biomes.OCEAN};
+	    	if (jsonobject2.has("biomes")) {
+	    		JsonArray jsonarray = jsonobject2.getAsJsonArray("biomes");
+	    		abiome = jsonarray.size() > 0 ? new Biome[jsonarray.size()] : new Biome[]{Biomes.OCEAN};
+	
+	    		for(int i = 0; i < jsonarray.size(); ++i) {
+	    			abiome[i] = Registry.BIOME.getValue(new ResourceLocation(jsonarray.get(i).getAsString())).orElse(Biomes.OCEAN);
+	    		}
+	    	}
+	
+	    	if (BiomeProviderType.FIXED == biomeprovidertype3) {
+	    		SingleBiomeProviderSettings singlebiomeprovidersettings2 = biomeprovidertype.createSettings(this.world.getWorldInfo()).setBiome(abiome[0]);
+	    		biomeprovider = biomeprovidertype.create(singlebiomeprovidersettings2);
+	    	}
+	
+	    	if (BiomeProviderType.CHECKERBOARD == biomeprovidertype3) {
+	    		int j = jsonobject2.has("size") ? jsonobject2.getAsJsonPrimitive("size").getAsInt() : 2;
+	    		CheckerboardBiomeProviderSettings checkerboardbiomeprovidersettings = biomeprovidertype2.createSettings(this.world.getWorldInfo()).setBiomes(abiome).setSize(j);
+	    		biomeprovider = biomeprovidertype2.create(checkerboardbiomeprovidersettings);
+	    	}
+	
+	    	if (BiomeProviderType.VANILLA_LAYERED == biomeprovidertype3) {
+	    		OverworldBiomeProviderSettings overworldbiomeprovidersettings1 = biomeprovidertype1.createSettings(this.world.getWorldInfo());
+	    		biomeprovider = biomeprovidertype1.create(overworldbiomeprovidersettings1);
+	    	}
+	    }
+	
+	    if (biomeprovider == null) {
+	    	biomeprovider = biomeprovidertype.create(biomeprovidertype.createSettings(this.world.getWorldInfo()).setBiome(Biomes.OCEAN));
+	    }
+	
+		BlockState stoneState = Blocks.STONE.getDefaultState();
+		BlockState waterState = Blocks.WATER.getDefaultState();
 		JsonObject jsonobject3 = jsonobject.getAsJsonObject("chunk_generator");
 		if (jsonobject3 != null && jsonobject3.has("options")) {
 			JsonObject jsonobject4 = jsonobject3.getAsJsonObject("options");
 		    if (jsonobject4.has("default_block")) {
 		    	String s = jsonobject4.getAsJsonPrimitive("default_block").getAsString();
-		    	blockstate = Registry.BLOCK.getOrDefault(new ResourceLocation(s)).getDefaultState();
+		    	stoneState = Registry.BLOCK.getOrDefault(new ResourceLocation(s)).getDefaultState();
 		    }
 		
 		    if (jsonobject4.has("default_fluid")) {
 		    	String s1 = jsonobject4.getAsJsonPrimitive("default_fluid").getAsString();
-		    	blockstate1 = Registry.BLOCK.getOrDefault(new ResourceLocation(s1)).getDefaultState();
+		    	waterState = Registry.BLOCK.getOrDefault(new ResourceLocation(s1)).getDefaultState();
 		    }
 		}
-		
-		if (jsonobject3 != null && jsonobject3.has("type")) {
-		    ChunkGeneratorType<?, ?> chunkgeneratortype5 = Registry.CHUNK_GENERATOR_TYPE.getOrDefault(new ResourceLocation(jsonobject3.getAsJsonPrimitive("type").getAsString()));
-		    if (ChunkGeneratorType.CAVES == chunkgeneratortype5) {
-		    	NetherGenSettings nethergensettings = chunkgeneratortype2.createSettings();
-		    	nethergensettings.setDefaultBlock(blockstate);
-		    	nethergensettings.setDefaultFluid(blockstate1);
-		    	return chunkgeneratortype2.create(this.world, biomeprovider, nethergensettings);
-		    }
-		
-		    if (ChunkGeneratorType.FLOATING_ISLANDS == chunkgeneratortype5) {
-		       	EndGenerationSettings endgenerationsettings = chunkgeneratortype3.createSettings();
-		       	endgenerationsettings.setSpawnPos(new BlockPos(0, 64, 0));
-		       	endgenerationsettings.setDefaultBlock(blockstate);
-		       	endgenerationsettings.setDefaultFluid(blockstate1);
-		       	return chunkgeneratortype3.create(this.world, biomeprovider, endgenerationsettings);
-		    }
-		}
-		
-		OverworldGenSettings overworldgensettings1 = chunkgeneratortype4.createSettings();
-		overworldgensettings1.setDefaultBlock(blockstate);
-		overworldgensettings1.setDefaultFluid(blockstate1);
-		return chunkgeneratortype4.create(this.world, biomeprovider, overworldgensettings1);
-		}
+	
+		OverworldGenSettings overworldgensettings1 = overworldGenerator.createSettings();
+		overworldgensettings1.setDefaultBlock(stoneState);
+		overworldgensettings1.setDefaultFluid(waterState);
+		return overworldGenerator.create(this.world, biomeprovider, overworldgensettings1);
 	}
 
 	@Override
@@ -223,7 +182,8 @@ public class UnderneathDimension extends Dimension {
 	      f1 = f1 * (f * 0.94F + 0.06F);
 	      f2 = f2 * (f * 0.94F + 0.06F);
 	      f3 = f3 * (f * 0.91F + 0.09F);
-	      return new Vec3d((double)f1, (double)f2, (double)f3);
+	      //return new Vec3d((double)f1, (double)f2, (double)f3);
+	      return new Vec3d(1, 1, 1);
 	}
 
 	@Override
@@ -233,7 +193,7 @@ public class UnderneathDimension extends Dimension {
 
 	@Override
 	public boolean doesXZShowFog(int x, int z) {
-		return false;
+		return true;
 	}
 
 }
